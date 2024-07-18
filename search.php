34be0e9bd8d6a437e8663179ba53216f20035075
DilshadSheikh <dilshadnoshad2003@gmail.com>
2024-07-18 21:59:29 +0500

    search part 3 done

--- search.php
+++ search.php
@@ -2,19 +2,25 @@
 <div id="primary">
     <div id="main">
         <div class="container">
+
+		<h1>Search Results for: <?php echo get_search_query() ?> </h1>
             <?php
+
+get_search_form();
 // already in the post no need to write if have post
 while (have_posts()):
     the_post();
     ?>
-				<article id="post-<?php the_ID();?>" <?php post_class();?> >
-			    <header>
-			        <h2><a href="<?php the_permalink()?>"><?php the_title()?></a></h2>
-			        <div class="meta-info">
-			            <p>Posted in <?php echo get_the_date() ?> by <?php the_author_posts_link();?></p>
-		                <p>Categories: <?php the_category(" ")?></p>
-		                <p>Tags <?php the_tags("", ", ")?></p>
-			        </div>
+						<article id="post-<?php the_ID();?>" <?php post_class();?> >
+					    <header>
+					        <h2><a href="<?php the_permalink()?>"><?php the_title()?></a></h2>
+					       <?php if ("post" == get_post_type()): ?>
+						   <div class="meta-info">
+					            <p>Posted in <?php echo get_the_date() ?> by <?php the_author_posts_link();?></p>
+				                <p>Categories: <?php the_category(" ")?></p>
+				                <p>Tags <?php the_tags("", ", ")?></p>
+					        </div>
+							<?php endif;?>
 			    </header>
 			    <div class="content">
 			        <?php the_excerpt()?>
