fd340c7665d0b2c4c469e0e1dfaa3e9bdb423119
DilshadSheikh <dilshadnoshad2003@gmail.com>
2024-10-04 13:36:33 +0500

    upload child-theme files

--- footer.php
+++ footer.php
@@ -1,9 +1,6 @@
 <footer class="site-footer">
     <div class="container">
-        <div class="copyright">
-            <!-- <p>Copyright &copy; - All Rights Reserved</p> -->
-             <p><?php echo esc_html(get_theme_mod("set_copyright", __("Copyright &copy; - All Rights Reserved", "iteducation"))) ?></p>
-        </div>
+        
         <nav class="footer-menu">
             <?php wp_nav_menu(array("theme_location" => "iteducation_footer_menu", "depth" => 1))?>
         </nav>
